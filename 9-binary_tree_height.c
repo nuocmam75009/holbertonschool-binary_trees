bf32da11355fde3486127e346ecd64c0715fb43d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-04-12 10:00:05 +0200

    debugged task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
 }
